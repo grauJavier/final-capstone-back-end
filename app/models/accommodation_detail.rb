1f853b98f4ac987a095a337d5a2c93cae0415b8f
Manuel Alejandro Sánchez Sierra <manuelsanchez059@gmail.com>
2023-10-26 12:20:10 -0500

    Change the associations between accommodation and details

--- app/models/accommodation_detail.rb
+++ app/models/accommodation_detail.rb
@@ -1,6 +1,4 @@
 class AccommodationDetail < ApplicationRecord
-  belongs_to :accommodation
-
   validates :accommodation, presence: true
   validates :accommodation_type, presence: true
   validates :bedrooms, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 1 }
