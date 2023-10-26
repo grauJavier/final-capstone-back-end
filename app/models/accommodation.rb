268dba277237713e156b1734ff5cc98231af8e65
lRebornsl <anthony_vasquez98@hotmail.com>
2023-10-26 11:38:04 -0500

    Fix linters

--- app/models/accommodation.rb
+++ app/models/accommodation.rb
@@ -2,5 +2,5 @@ class Accommodation < ApplicationRecord
   belongs_to :details
 
   validates :name, presence: true, length: { minimum: 3, maximum: 255 }
-  validates :image_url, presence: true, format: { with: /\Ahttps?:\/\/\S+\z/, message: "Invalid URL format" }
+  validates :image_url, presence: true, format: { with: %r{\Ahttps?://\S+\z}, message: 'Invalid URL format' }
 end
--- test/models/accommodation_detail_test.rb
+++ test/models/accommodation_detail_test.rb
@@ -1,4 +1,4 @@
-require "test_helper"
+require 'test_helper'
 
 class AccommodationDetailTest < ActiveSupport::TestCase
   # test "the truth" do
