5422ba9501aad93575721aa47d2e5c5f2de4bddd
Manuel Alejandro Sánchez Sierra <manuelsanchez059@gmail.com>
2023-10-26 11:37:21 -0500

    Add user validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :reservations
+
+  validates :username, presence: true, length: { maximum: 150 }
 end
