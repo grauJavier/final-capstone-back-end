e381ecfda286e2c05fed227a283177cbcb23fe2d
lRebornsl <anthony_vasquez98@hotmail.com>
2023-10-27 12:12:56 -0500

    Fix linters

--- app/controllers/cities_controller.rb
+++ app/controllers/cities_controller.rb
@@ -2,18 +2,18 @@ class CitiesController < ApplicationController
   before_action :find_reservation, only: [:show]
 
   def index
-      cities = City.all
-      render json: cities
+    cities = City.all
+    render json: cities
   end
 
   def show
-      city = City.find(@reservation.city_id)
-      render json: city
+    city = City.find(@reservation.city_id)
+    render json: city
   end
 
   private
 
   def find_reservation
-      @reservation = Reservation.find(params[:reservation_id])
+    @reservation = Reservation.find(params[:reservation_id])
   end
 end
