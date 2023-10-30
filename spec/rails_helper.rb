e9f5820cfe3e6b0697739ed7949bad937e7868a2
Javier Grau <javiergrau.freelance@gmail.com>
2023-10-30 16:19:49 -0300

    Linters Fix

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -64,8 +64,8 @@ RSpec.configure do |config|
   # config.filter_gems_from_backtrace("gem name")
 
   require 'shoulda/matchers'
-  Shoulda::Matchers.configure do |config|
-    config.integrate do |with|
+  Shoulda::Matchers.configure do |configuration|
+    configuration.integrate do |with|
       with.test_framework :rspec
       with.library :rails
     end
