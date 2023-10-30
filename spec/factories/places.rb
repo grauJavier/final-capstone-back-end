6037024440562da25b2b89f94bbb6b8b186dfbe0
Javier Grau <javiergrau.freelance@gmail.com>
2023-10-30 17:07:33 -0300

    Fix name length

--- spec/factories/places.rb
+++ spec/factories/places.rb
@@ -2,7 +2,7 @@ require 'faker'
 
 FactoryBot.define do
   factory :place do
-    name { Faker::Lorem.word }
+    name { Faker::Lorem.words(number: 3).join(' ') }
     image_url { Faker::Internet.url }
     description { Faker::Lorem.sentence }
     user
