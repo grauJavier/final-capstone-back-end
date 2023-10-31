b3112ff15bf04b161c526666323be176f19868a9
Manuel Alejandro Sánchez Sierra <manuelsanchez059@gmail.com>
2023-10-31 14:48:39 -0500

    Change models associations

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,8 +1,10 @@
 class Place < ApplicationRecord
   belongs_to :user
   has_one :detail, dependent: :destroy
+  belongs_to :city
 
   validates :name, presence: true, length: { minimum: 3, maximum: 255 }
   validates :image_url, presence: true, format: { with: %r{\Ahttps?://\S+\z}, message: 'Invalid URL format' }
   validates :description, presence: true, length: { minimum: 3, maximum: 255 }
+  validates :city, presence: true
 end
