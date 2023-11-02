95f1f57164a4d24aab8c52ce933e04d53bd97093
Javier Grau <javiergrau.freelance@gmail.com>
2023-11-02 01:22:11 -0300

    Modify to create one 'Place' per 'City'

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,21 +1,11 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+# Seed User
+user = User.find_or_create_by!(email: 'graujavier@gmail.com') do |u|
+  u.username = 'Javier'
+  u.password = '123456'
+end
 
-# User
-user = User.create!(username: 'Javier', email:'graujavier@gmail.com', password:'123456')
-
-# Place
-image_url = 'https://cf.bstatic.com/xdata/images/hotel/max1024x768/131210897.jpg?k=96dae0b1bb492a08ca3d2983d4c0f9ab91da5cf75d0921a1267160524a5b4d58&o=&hp=1'
-
-# City
-cities = [
+# Seed Cities
+cities_data = [
   'New York City, USA',
   'London, UK',
   'Paris, France',
@@ -33,17 +23,18 @@ cities = [
   'Moscow, Russia'
 ]
 
-cities.each do |city_name|
-  City.create!(name: city_name)
+cities_data.each do |city_name|
+  city = City.find_or_create_by!(name: city_name)
+
+  # Seed a place for each city
+  image_url = "https://example.com/#{city_name.parameterize}"
+  place_name = "Hotel in #{city_name}"
+  
+  Place.find_or_create_by!(name: place_name, city: city) do |place|
+    place.user = user
+    place.image_url = image_url
+    place.description = "Description of #{place_name}"
+  end
 end
-
-city = City.find_by(name: 'Tokyo, Japan')
-
-place = Place.create!(user: user, city: city, name: 'Hotel', image_url: image_url, description: 'Hotel description');
-
-Detail.create!(place: place, place_type: 'Room', bedrooms: 2, beds: 2, bathrooms: 1, property_type: 'Hotel', price: 120);
-
-# Reservation
-Reservation.create!(user: user, place: place, schedule_date:'2023-12-12')
 
 puts 'Seeds created successfully!'
