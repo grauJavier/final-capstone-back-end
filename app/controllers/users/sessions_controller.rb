baffa313cfc22a64b76c14b9a477004ad85f22a9
Manuel Alejandro Sánchez Sierra <manuelsanchez059@gmail.com>
2023-11-14 15:58:18 -0500

    Modify user devise jwt token

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -15,8 +15,7 @@ class Users::SessionsController < Devise::SessionsController
 
   def respond_to_on_destroy
     if request.headers['Authorization'].present?
-      jwt_payload = JWT.decode(request.headers['Authorization'].split.last,
-                               Rails.application.credentials.devise_jwt_secret_key!).first
+      jwt_payload = JWT.decode(request.headers['Authorization'].split.last, ENV['DEVISE_JWT_SECRET_KEY']).first
       current_user = User.find(jwt_payload['sub'])
     end
 
